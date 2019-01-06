a161322d10b24cdc90a0c618dedb723fa20f88b4
Ruyi Li <ruyili2002@gmail.com>
2019-01-06 17:42:28 -0500

    ftfy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Discord = require ('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json')
 client.on('ready',() => {
-    console.log('I\'m Online);
+    console.log('I\'m Online');
 })
 
 client.on('message', message => {
