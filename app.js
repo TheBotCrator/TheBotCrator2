873fce34d947f4f4457a085cc4c577f272e9cc0e
Harim Ahmed <harimahmed04@gmail.com>
2019-01-06 10:40:03 -0700

    Added settings.json to .gitignore, created app.js and settings.json.

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require ('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json')
+client.on('ready',() => {
+    console.log('I\'m Online);
+})
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('Pong!');
+
+    }
+});
+
+client.login(settings.token);
